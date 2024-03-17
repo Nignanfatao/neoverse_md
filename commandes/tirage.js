1867ab0cb379221643f1137e9920d167f4f9acb6
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-03-17 18:48:51 +0000

    Update tirage.js

--- commandes/tirage.js
+++ commandes/tirage.js
@@ -1,40 +1,112 @@
 const { zokou } = require('../framework/zokou');
 
 //debut tirage card 💎
-const ssm = [];
-const ss = [];
-const ssp = [];
-const sspsuper = [];
-const sspmega = [];
-const sspultra = [];
+const ssm = ['https://telegra.ph/file/010fd43c008e41fe5a5c2.jpg' // super buu ss-
+];
+const ss = ['https://telegra.ph/file/231c0229ca71f9522278f.jpg', // Vegeta Ss¹1
+'https://telegra.ph/file/2d59f58d786cd111688cf.jpg', // goku ss¹1
+'https://telegra.ph/file/385eb54c3480d52c5273d.jpg' // buutrunks ss1
+ 
+];
+const ssp = ['https://telegra.ph/file/2e7022832c3d2bd724045.jpg', // aizen S+1
+'https://telegra.ph/file/58153d6d5087030ec05c3.jpg',  //Eida S+1
+'https://telegra.ph/file/f77356be0b92c11c1e7da.jpg', // Daemon S+1
+'https://telegra.ph/file/e8fb0aae19bc8ed49454d.jpg' // Lemiel s+1
+ ];
+const sspsuper = [ 'https://telegra.ph/file/f537d55a26541a6ab3b2a.jpg', // hypnos ss+super1
+                   'https://telegra.ph/file/48e8e1e17c2282c94a93a.jpg', // Thanatos Ss+super1
+                   'https://telegra.ph/file/c72549456d40524b16da7.jpg', // Hit Ss+super1
+                   'https://telegra.ph/file/be047c496050a978671a2.jpg', // Goku sssuper¹1
+                   'https://telegra.ph/file/496db45cbaf598c2089c6.jpg', // Cabba Ss+super3
+                   'https://telegra.ph/file/f36573d88a16827a1ff66.jpg' // Goku Ss+super²1
+ 
+];
+const sspmega = ['https://telegra.ph/file/16c676be951d8f70d6d09.jpg', // Kefla Ss+ Mega3
+             'https://telegra.ph/file/ecea10db60962562ea97f.jpg', // Jiren Ss+ Mega²1
+             'https://telegra.ph/file/28d91fcd1102b520517d1.jpg', // Adam Ss+ mega1
+             'https://telegra.ph/file/19a0fcde7475e02906b66.jpg' // superbuuss-1
+ 
+ 
+];
+const sspultra = ['https://telegra.ph/file/00274b68634f9f037b23d.jpg' //limule ss+ultra1
+ 
+];
 const sspultimate = [];
  // fin tirage card 💎
 
  //debut tirage 🥇
  const sm1 = ['https://telegra.ph/file/426754902fd6c891f7af5.jpg',
               'https://telegra.ph/file/a21dfbda488fd0f52de69.jpg',
-              'https://telegra.ph/file/39b375e4a8f88051ccdb9.jpg'
+              'https://telegra.ph/file/39b375e4a8f88051ccdb9.jpg',
+              'https://telegra.ph/file/03e95776d943a8038c417.jpg', //Aiolia s-1
+              'https://telegra.ph/file/24c5fadef57ea3561f344.jpg', //Patalli s-1
+              'https://telegra.ph/file/4cda2c38667cb6e84ff3d.jpg', // Naruto(the last movie) S-1
+              'https://telegra.ph/file/1a78898c7f42a2d9e3a07.jpg', // Giorno S-1
+              'https://telegra.ph/file/370741769428ee2cb6a59.jpg' //Sun Jin Woo s-1
              ];
- const s1 = [];
- const sp1 = [];
- const spsuper1 = [];
+ const s1 = ['https://telegra.ph/file/6148a475bbdfb3eb987c5.jpg', //Mu S1
+'https://telegra.ph/file/5bfd3a977f5b7aa2ba072.jpg', //code S1
+'https://telegra.ph/file/a033749578cfdeb6038b2.jpg', // Madara Six paths(mugen tsukuyomi) S1
+  'https://telegra.ph/file/06f5adeb35821f28e117c.jpg'  // yamamoto s1           
+  
+ ];
+ const sp1 = ['https://telegra.ph/file/9b5ef6db91043b5db0484.jpg'
+  
+ ];
+ const spsuper1 = ['https://telegra.ph/file/62309005b59b8a397fc1a.jpg', // Ichigo S+super
+                   'https://telegra.ph/file/b7e32c2bc6c1e2e5a673b.jpg' // cabba ss+super1
+  
+ ];
 //fin tirage 🥇
 
 // debut tirage 🥈
  const sm2 = ['https://telegra.ph/file/672a24193384b4bd8e1fd.jpg',
               'https://telegra.ph/file/86921b5a9ef7da3c14bd0.jpg',
-              'https://telegra.ph/file/cfcbfa4137e6eb8f4de66.jpg'
+              'https://telegra.ph/file/cfcbfa4137e6eb8f4de66.jpg',
+              'https://telegra.ph/file/bebac36db3bf74c021d22.jpg', // Netero s-2
+'https://telegra.ph/file/e320a2fcfead7ef3c6423.jpg', // Kashimo S-2
+'https://telegra.ph/file/c12fcfb537023861feec9.jpg', //Maki s-2
+'https://telegra.ph/file/50857f071986a80b12f5b.jpg', // Yuki s-2
+'https://telegra.ph/file/c71a40e2b3f512947fac6.jpg', // uraume s-2
+'https://telegra.ph/file/f77bbb0ee531a107da1f5.jpg', // Marco S-2
+'https://telegra.ph/file/9fad143b4b91245f37013.jpg', //Luffy s-2
+'https://telegra.ph/file/f075991cfda1c6d234572.jpg', // gremmy s-
+'https://telegra.ph/file/6c5b436382383f7955613.jpg', // Yuta s-2
+'https://telegra.ph/file/eb31b4ea7bd082709bfa9.jpg', //Mahoraga s-2
+'https://telegra.ph/file/ac7663e4ea835c468fde7.jpg', //Mahito s-2
+'https://telegra.ph/file/6880e8cfc8cb408c6a622.jpg', // Kenjaku s-2
+'https://telegra.ph/file/9fef6c5728d2cb63f933b.jpg', // Itadori S-2
+'https://telegra.ph/file/a4318001b24d91997e893.jpg', // Doflamingo s-2
+    'https://telegra.ph/file/9bc8658bd275f1d122b96.jpg' // mash s-2          
               ];
- const s2 = [];
- const sp2 = [];
- const spsuper2 = [];
+ const s2 = ['https://telegra.ph/file/2804c718042a3b329a65b.jpg',// Flashy flash s2
+            ' https://telegra.ph/file/84eedf7a4bb4b1c6da680.jpg', // luffy(wano arc) s2
+             'https://telegra.ph/file/47842bb500bace9724f79.jpg' // Grimmjow s2
+  
+ ];
+ const sp2 = ['https://telegra.ph/file/a0828fe4416950d801d72.jpg', //ulquiorra s+2
+'https://telegra.ph/file/bd2f3d1bd6d709be3cdaf.jpg', // izuku s+2
+'https://telegra.ph/file/74631889fa24a3c8c28ea.jpg', // cid s+2
+'https://telegra.ph/file/1064a245a87122f6b8fb4.jpg', // garp s+2
+'https://telegra.ph/file/0785892951c478cebb3c8.jpg', //Mihawk s+2
+'https://telegra.ph/file/61429b2f94de5e9bb1730.jpg'  // katakuri s+2
+  
+ ];
+ const spsuper2 = ['https://telegra.ph/file/2f3c9b65d600c4d5fd8b7.jpg', // big mom s+super2
+'https://telegra.ph/file/ae98b09ff6d6290d1069d.jpg', // sukuna s+super2
+'https://telegra.ph/file/664d29d2eca99db29eebd.jpg' // hitsugaya ssuper2
+  
+ ];
 //Fin tirage 🥈
 
  
  // debut tirage 🥉
  const sm3 = ['https://telegra.ph/file/94381c3e145584c2ab242.jpg',
               'https://telegra.ph/file/7b4cf81d7b513ea55cf5e.jpg',
-              'https://telegra.ph/file/eb19ddad296b886852364.jpg'
+              'https://telegra.ph/file/eb19ddad296b886852364.jpg',
+              'https://telegra.ph/file/cd226da3550fd5e92186d.jpg', // polnareff s-3
+'https://telegra.ph/file/782f74aa3ef5263fdb226.jpg', //dot s-3
+'https://telegra.ph/file/4cfeb4dca9a0e6d313c6c.jpg' // giyu s-3
               ];
  const s3 = ['https://telegra.ph/file/7a9aa8472b2d0a5fdbce7.jpg',
              'https://telegra.ph/file/333e26a425921aa0fbd28.jpg',
@@ -44,12 +116,27 @@ const sspultimate = [];
              'https://telegra.ph/file/d7728b4458bfe6eed9051.jpg',
              'https://telegra.ph/file/46b20b4ccb6ca65993d1d.jpg',
              'https://telegra.ph/file/292a10e8928829ee7de07.jpg',
-             'https://telegra.ph/file/b497b91dd06bebf7f208e.jpg'
+             'https://telegra.ph/file/b497b91dd06bebf7f208e.jpg',
+             'https://telegra.ph/file/bd0d7ee9b6dd2d9d2839e.jpg', //Shin s3
+'https://telegra.ph/file/f593a374bd7458f073483.jpg', // choso S3
+'https://telegra.ph/file/73ea51f3cd6f3977cb8d4.jpg', // Naobito s3
+'https://telegra.ph/file/5b4f4a4b754e98881c296.jpg', //joker s3
+'https://telegra.ph/file/cf8bb18a3d4e0f80f1ec0.jpg', //Yuji s3
+'https://telegra.ph/file/8d2cb6a82e81bbb36a2b9.jpg', //Kakyoin s3
+'https://telegra.ph/file/56cd208e8de6e8ecd3eee.jpg', // avdol s3
+'https://telegra.ph/file/4863985488f36ad717455.jpg', //Lance s3
+  'https://telegra.ph/file/9ec7f0325d34f50804d90.jpg' // yamcha s3
              ];
  const sp3 = ['https://telegra.ph/file/53e00960b21589b85981b.jpg',
-              'https://telegra.ph/file/a26e4fa9d5f0dda2a5a60.jpg'
+              'https://telegra.ph/file/a26e4fa9d5f0dda2a5a60.jpg',
+              'https://telegra.ph/file/cf0840a79020b45d919ee.jpg', //Aoi s+3
+'https://telegra.ph/file/284191b07d77d6ab6da46.jpg' //sukuna s+3
               ];
-  const spsuper3 = [];
+  const spsuper3 = ['https://telegra.ph/file/18b45cf94f8c6a93630f3.jpg', //tenshihan s+utlimate3
+'https://telegra.ph/file/e799faa603819bfd93790.jpg', //krilin s+ultimate3
+'https://telegra.ph/file/e97d45d6422b5d3f74aba.jpg', // hakari s+super3
+   
+  ];
  // fin tirage 🥉
    
 
@@ -98,62 +185,62 @@ zokou(
                 const p1 = tirerCarte(ss_pourcentage, 1.5);
                 if (p1  === 4) {
                     zk.sendMessage(dest, { image: { url: lien1 } }, { quoted: ms });
-                  await sleep(3000);
+                  sleep(3000);
                  lien();
-                    zk.sendMessage(dest, { image: { url: lien1 } }, { quoted: ms });
+                   await zk.sendMessage(dest, { image: { url: lien1 } }, { quoted: ms });
                lien(); } else if (p1 === 3) {
                     zk.sendMessage(dest, { image: { url: lien2 } }, { quoted: ms });
-                   await sleep(3000);
+                    sleep(3000);
                  lien();
-                     zk.sendMessage(dest, { image: { url: lien2 } }, { quoted: ms });
+                    zk.sendMessage(dest, { image: { url: lien2 } }, { quoted: ms });
              lien();   } else if (p1 === 2) {
                     zk.sendMessage(dest, { image: { url: lien3 } }, { quoted: ms });
-                  await sleep(3000);
+                  sleep(3000);
                  lien();
                       zk.sendMessage(dest, { image: { url: lien3 } }, { quoted: ms });
                lien(); } else if (p1 === 1.5) {
                     zk.sendMessage(dest, { image: { url: lien4 } }, { quoted: ms });
-                  await sleep(3000);
+                   sleep(3000);
                  lien();
                       zk.sendMessage(dest, { image: { url: lien4 } }, { quoted: ms });
               lien();  } else if (p1 === 1) {
                     zk.sendMessage(dest, { image: { url: lien5 } }, { quoted: ms });
-               await sleep(3000);
+               sleep(3000);
                  lien();
                          zk.sendMessage(dest, { image: { url: lien5 } }, { quoted: ms });
                lien(); } else if (p1 === 0.5) {
                     zk.sendMessage(dest, { image: { url: lien6 } }, { quoted: ms });
-                   await sleep(3000);
+                   sleep(3000);
                  lien();
                      zk.sendMessage(dest, { image: { url: lien6 } }, { quoted: ms });
                lien(); } else {
                     zk.sendMessage(dest, { image: { url: lien7 } }, { quoted: ms });
-                  await sleep(3000);
+                  sleep(3000);
                  lien();
                       zk.sendMessage(dest, { image: { url: lien7 } }, { quoted: ms });
             lien();    }
                 break;
             case "🥇":
                 const le_pourcentage = [7, 5, 3, 2];
-                const p2 = tirerCarte(le_pourcentage, 1.5);
+                const p2 = tirerCarte(le_pourcentage, 1.5);figure 
                 if (p2 === 5) {
                     zk.sendMessage(dest, { image: { url: lien8 } }, { quoted: ms });
-                 await sleep(3000);
+                  sleep(3000);
                  lien();
                        zk.sendMessage(dest, { image: { url: lien8 } }, { quoted: ms });
                lien(); } else if (p2 === 3) {
                     zk.sendMessage(dest, { image: { url: lien9 } }, { quoted: ms });
-                 await sleep(3000);
+                  sleep(3000);
                  lien();
                        zk.sendMessage(dest, { image: { url: lien9 } }, { quoted: ms });
                lien(); } else if (p2 === 2) {
                     zk.sendMessage(dest, { image: { url: lien10 } }, { quoted: ms });
-                await sleep(3000);
+                 sleep(3000);
                  lien();
                         zk.sendMessage(dest, { image: { url: lien10 } }, { quoted: ms });
               lien();  } else {
                     zk.sendMessage(dest, { image: { url: lien11 } }, { quoted: ms });
-               await sleep(3000);
+               sleep(3000);
                  lien();
                          zk.sendMessage(dest, { image: { url: lien11 } }, { quoted: ms });
             lien();    }
@@ -163,22 +250,22 @@ zokou(
           const p3 = tirerCarte(_pourcentage, 1.5);
                 if (p3 === 30) {
                     zk.sendMessage(dest, { image: { url: lien12 } }, { quoted: ms });
-              await sleep(3000);
+              sleep(3000);
                  lien();
                           zk.sendMessage(dest, { image: { url: lien12 } }, { quoted: ms });
              lien();   } else if (p3 === 20) {
                     zk.sendMessage(dest, { image: { url: lien13 } }, { quoted: ms });
-               await sleep(3000);
+                sleep(3000);
                  lien();
                          zk.sendMessage(dest, { image: { url: lien13 } }, { quoted: ms });
             lien();    } else if (p3 === 10) {
                     zk.sendMessage(dest, { image: { url: lien14 } }, { quoted: ms });
-               await sleep(3000);
+                sleep(3000);
                  lien();
                          zk.sendMessage(dest, { image: { url: lien14 } }, { quoted: ms });
              lien();   } else {
                     zk.sendMessage(dest, { image: { url: lien15 } }, { quoted: ms });
-           await sleep(3000);
+           sleep(3000);
                  lien();
                              zk.sendMessage(dest, { image: { url: lien15 } }, { quoted: ms });
               lien();  }
@@ -188,22 +275,22 @@ zokou(
                 const p4 = tirerCarte(sp_pourcentage, 1.5);
                 if (p4 === 70) {
                     zk.sendMessage(dest, { image: { url: lien16 } }, { quoted: ms });
-             await sleep(3000);
+              sleep(3000);
                  lien();
                            zk.sendMessage(dest, { image: { url: lien16 } }, { quoted: ms });
              lien();   } else if (p4 === 60) {
                     zk.sendMessage(dest, { image: { url: lien17 } }, { quoted: ms });
-              await sleep(3000);
+              sleep(3000);
                  lien();
                           zk.sendMessage(dest, { image: { url: lien17 } }, { quoted: ms });
             lien();    } else if (p4 === 50) {
                     zk.sendMessage(dest, { image: { url: lien18 } }, { quoted: ms });
-           await sleep(3000);
+           sleep(3000);
                  lien();
                              zk.sendMessage(dest, { image: { url: lien18 } }, { quoted: ms });
                lien(); } else {
                     zk.sendMessage(dest, { image: { url: lien19 } }, { quoted: ms });
-           await sleep(3000);
+           sleep(3000);
                  lien();
                              zk.sendMessage(dest, { image: { url: lien19 } }, { quoted: ms });
           lien();      } 
@@ -224,7 +311,7 @@ function tirerCarte(listePourcentages, facteurAttenuation) {
     const sommePourcentages = listePourcentages.reduce((acc, pourcentage) => acc + pourcentage, 0);
     
     // Calculer le facteur d'atténuation inverse (pour inverser l'effet sur les pourcentages)
-    const inverseFacteurAttenuation = 1 / facteurAttenuation;
+    const inverseFacteurAttenuation = 0.2 / facteurAttenuation;
     
     // Générer un nombre aléatoire entre 0 et la somme des pourcentages
     const randomValue = Math.random() * sommePourcentages;
