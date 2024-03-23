a292731578d4ef88ef5d1ccca73a891e78bee6fd
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-03-23 21:22:41 +0000

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -1,7 +1,7 @@
 const { zokou } = require('../framework/zokou');
 const traduire = require("../framework/traduction") ;
 const fs = require('fs-extra');
-const s = require('./set');
+const s = require('../set');
 const axios = require('axios');
 
 
