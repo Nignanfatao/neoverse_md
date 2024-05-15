3edc52a33e82bd9b24d97e87280cbac4bbc2c5c9
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-05-15 01:35:58 +0000

    Update elysiumfiche.js

--- bdd/elysiumfiche.js
+++ bdd/elysiumfiche.js
@@ -102,11 +102,11 @@ async function getData(ide) {
 
 // Appeler la fonction createNorth1FicheTable après la création de la table
 createElysiumFicheTable();
-insertData();
+//insertData();
 
 module.exports = {
   createElysiumFicheTable,
  // insertData1,
-  insertData,
+ // insertData,
   getData
 };
