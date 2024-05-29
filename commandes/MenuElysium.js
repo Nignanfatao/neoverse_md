8b8a8290cf07b8ca2c8d7b4e121358dbd600961c
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-05-29 13:43:09 +0000

    Update MenuElysium.js

--- commandes/MenuElysium.js
+++ commandes/MenuElysium.js
@@ -3,7 +3,7 @@ const { zokou } = require('../framework/zokou');
 
 zokou(
     {
-        nomCom: '',
+        nomCom: 'menuelysium💠',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -12,7 +12,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/1cc788ca14b06a219af36.jpg }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -20,7 +20,7 @@ zokou(
 
 zokou(
     {
-        nomCom: '',
+        nomCom: 'histoiredebase💠',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -29,7 +29,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/69294de81c58878ca7bcc.jpg }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -37,7 +37,7 @@ zokou(
 
 zokou(
     {
-        nomCom: '',
+        nomCom: 'guidedujeu💠',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -46,7 +46,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/93608ec044aac73ee2ff1.jpg }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -54,7 +54,7 @@ zokou(
 
 zokou(
     {
-        nomCom: '',
+        nomCom: 'creationdelavatar💠',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -63,7 +63,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/da983a81f14dbbc3cef4f.jpg }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -71,7 +71,7 @@ zokou(
 
 zokou(
     {
-        nomCom: '',
+        nomCom: 'competencesetcombat💠',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -80,7 +80,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/bd863e7a4cc419236c4b0.jpg }, caption: msg }, { quoted: ms });
    
         }
     }
