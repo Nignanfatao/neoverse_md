70df33d82658bd1f78603b89362645d0165bd438
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-05-29 13:49:01 +0000

    Update MenuElysium.js

--- commandes/MenuElysium.js
+++ commandes/MenuElysium.js
@@ -3,7 +3,7 @@ const { zokou } = require('../framework/zokou');
 
 zokou(
     {
-        nomCom: 'menuelysium💠',
+        nomCom: 'menuelysium',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -12,7 +12,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/1cc788ca14b06a219af36.jpg }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/1cc788ca14b06a219af36.jpg' }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -20,7 +20,7 @@ zokou(
 
 zokou(
     {
-        nomCom: 'histoiredebase💠',
+        nomCom: 'histoiredebase',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -29,7 +29,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/69294de81c58878ca7bcc.jpg }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/69294de81c58878ca7bcc.jpg' }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -37,7 +37,7 @@ zokou(
 
 zokou(
     {
-        nomCom: 'guidedujeu💠',
+        nomCom: 'guidedujeu',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -46,7 +46,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/93608ec044aac73ee2ff1.jpg }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/93608ec044aac73ee2ff1.jpg' }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -54,7 +54,7 @@ zokou(
 
 zokou(
     {
-        nomCom: 'creationdelavatar💠',
+        nomCom: 'creationdelavatar',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -63,7 +63,7 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/da983a81f14dbbc3cef4f.jpg }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/da983a81f14dbbc3cef4f.jpg' }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -71,7 +71,7 @@ zokou(
 
 zokou(
     {
-        nomCom: 'competencesetcombat💠',
+        nomCom: 'competencesetcombat',
         categorie: 'NEOverse'
     },
     async (dest, zk, commandeOptions) => {
@@ -80,7 +80,24 @@ zokou(
         if (!arg || arg.length === 0)  {
             const lien = '';
             const msg = ``;
-            zk.sendMessage(dest, { image: { url: https://telegra.ph/file/bd863e7a4cc419236c4b0.jpg }, caption: msg }, { quoted: ms });
+            zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/bd863e7a4cc419236c4b0.jpg' }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+/*zokou(
+    {
+        nomCom: '',
+        categorie: 'NEOverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = '';
+            const msg = ``;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
    
         }
     }
@@ -101,21 +118,4 @@ zokou(
    
         }
     }
-);
-
-zokou(
-    {
-        nomCom: '',
-        categorie: 'NEOverse'
-    },
-    async (dest, zk, commandeOptions) => {
-        const { repondre, arg, ms } = commandeOptions;
-
-        if (!arg || arg.length === 0)  {
-            const lien = '';
-            const msg = ``;
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
-   
-        }
-    }
-);
+);*/
