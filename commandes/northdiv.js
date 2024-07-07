fc654a9ade5db9911b8dca4ba910845a6d489e7c
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-07-07 10:19:37 +0000

    Update northdiv.js

--- commandes/northdiv.js
+++ commandes/northdiv.js
@@ -429,7 +429,7 @@ zokou(
 ░░░░░░░░░░░░░░░░░░░
 ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
     *🔷𝗡Ξ𝗢 SUPERLEAGUE🏆🔝*`;
-zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/b15ca6adb54a20eba25f6.jpg' }, caption: mesg }, { quoted: ms });
+zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/56f8a1ed8a2390a5debf6.jpg' }, caption: mesg }, { quoted: ms });
        } else {
         if (superUser) { 
         //const dbUrl = "postgres://fatao:Kuz6KQRpz3S1swoTQTv1WOG8SPfSCppB@dpg-cmnlnkol5elc738lrj2g-a.oregon-postgres.render.com/cy";
@@ -772,7 +772,7 @@ zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/d7b251629aa05c5aff
       console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
     }
   });
-*/
+
 zokou(
   {
     nomCom: 'northkanzen👤',
@@ -900,7 +900,7 @@ zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/e521acd5939414d8d1
       console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
     }
   });
-
+*/
 zokou(
   {
     nomCom: 'northainz👤',
@@ -1414,6 +1414,518 @@ zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/1979a11043529f6ce2
             UPDATE northdiv
             SET ${colonneObjet} = $1
             WHERE id = 10
+            `;
+
+            await client.query(query, [texte]);
+
+            console.log(`données du joueur: ${joueur} mise à jour`);
+            await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${texte} \n *NOUVELLE DONNÉE*: ${texte}`);
+          } else {
+            console.log("Nom d'objet non reconnu ou signe invalide.");
+            repondre(`Une erreur est survenue. Veuillez entrer correctement les données.`);
+          }
+        } else {
+          console.log("Le message ne correspond pas au format attendu.");
+          repondre(`Le format du message est incorrect.`);
+        } 
+        } else { repondre('Seul les Membres de la NS ont le droit de modifier cette fiche');}
+       
+
+        client.release();
+      }
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
+    }
+  });
+
+zokou(
+  {
+    nomCom: 'northor👤',
+    categorie: 'NEOverse'
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, repondre, arg, superUser } = commandeOptions;
+
+    try {
+      const data = await getData('11');
+      let joueur = arg[1];
+      let object = arg[3];
+      let signe = arg[4];
+      let valeur = arg[5];
+      let texte = arg.slice(5).join(' ');
+
+      if (!arg || arg.length === 0) {
+        let mesg = `◇ *Pseudo👤*: ${data.e1}
+◇ *Division🛡️*: ${data.e2}
+◇ *Classe🏆*: ${data.e3}
+◇ *Rang XP🔰*: ${data.e4}
+◇ *Golds🧭*: ${data.e5}🧭
+◇ *NΞOcoins🔹*: ${data.e6}🔷
+◇ *Gift Box🎁*: ${data.e7}🎁
+◇ *Coupons🎟*: ${data.e8}🎟
+◇ *NΞO PASS🔸*: ${data.e9}🔸 
+*❯❯▓▓▓▓▓▓▓▓▓▓▓▓▓▓*
+ **🧠Talent Qi: ${data.e10}⭐* 
+*✅Clean games*:  ${data.e18}  *❌Mauvais PA:* ${data.e19}                             
+*👊🏻Close combat*: ${data.e20}     *🌀Attaques*: ${data.e21}
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*✭Records*: ${data.e12} Victoires✅/ ${data.e13} Défaites❌
+*🏆Trophées*: ${data.e14}  *🌟 TOS*: ${data.e15}  
+*💫Neo Awards*: ${data.e16}   *🎖️Globes*: ${data.e22}
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards*: ${data.e17} 
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+    *🔷𝗡Ξ𝗢 SUPERLEAGUE🏆🔝*`;
+zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/de942d6d04a11aa1002ae.jpg' }, caption: mesg }, { quoted: ms });
+       } else {
+        if (superUser) { 
+     //   const dbUrl = "postgres://fatao:Kuz6KQRpz3S1swoTQTv1WOG8SPfSCppB@dpg-cmnlnkol5elc738lrj2g-a.oregon-postgres.render.com/cy";
+        const proConfig = {
+          connectionString: dbUrl,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+
+        const { Pool } = require('pg');
+        const pool = new Pool(proConfig);
+        const client = await pool.connect();
+
+        if (arg[0] === 'joueur:') {
+          let colonnesJoueur;
+          
+          switch (joueur) {
+    case "Or":
+      colonnesJoueur = {
+        pseudo: "e1",
+        division: "e2",
+        classe: "e3",
+        rang_exp: "e4",
+        golds: "e5",
+        neocoins: "e6",
+        gift_box: "e7",
+        coupons: "e8",
+        neopass: "e9",
+        talent: "e10",
+        victoires: "e12",
+        defaites: "e13",
+        trophees: "e14",
+        tos: "e15",
+        awards: "e16",
+        cards: "e17",
+        Clean_games: "e18",
+        Mauvais_pa: "e19",
+        Close_combat: "e20",
+        Attaques: "e21",
+        globes: "e22"
+      };
+        break;
+          default:
+      console.log("Nom de joueur non reconnu.");
+              repondre(`joueur: ${joueur} non reconnu`);
+              return; 
+        }
+          
+        const colonneObjet = colonnesJoueur[object];
+        const solde = `${data[colonneObjet]} ${signe} ${valeur}`;
+
+          if (colonneObjet && (signe === '+' || signe === '-')) {
+            const query = `UPDATE northdiv SET ${colonneObjet} = ${data[colonneObjet]} ${signe} ${valeur} WHERE id = 11`;
+            await client.query(query);
+
+            console.log(`Données de l'utilisateur ${joueur} mises à jour`);
+           await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${signe}${valeur}\n*NOUVEAU SOLDE*: ${solde}`);
+          } else if (colonneObjet && signe === '=') {
+            const query = `
+            UPDATE northdiv
+            SET ${colonneObjet} = $1
+            WHERE id = 11
+            `;
+
+            await client.query(query, [texte]);
+
+            console.log(`données du joueur: ${joueur} mise à jour`);
+            await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${texte} \n *NOUVELLE DONNÉE*: ${texte}`);
+          } else {
+            console.log("Nom d'objet non reconnu ou signe invalide.");
+            repondre(`Une erreur est survenue. Veuillez entrer correctement les données.`);
+          }
+        } else {
+          console.log("Le message ne correspond pas au format attendu.");
+          repondre(`Le format du message est incorrect.`);
+        } 
+        } else { repondre('Seul les Membres de la NS ont le droit de modifier cette fiche');}
+       
+
+        client.release();
+      }
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
+    }
+  });
+
+zokou(
+  {
+    nomCom: 'northaruto👤',
+    categorie: 'NEOverse'
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, repondre, arg, superUser } = commandeOptions;
+
+    try {
+      const data = await getData('12');
+      let joueur = arg[1];
+      let object = arg[3];
+      let signe = arg[4];
+      let valeur = arg[5];
+      let texte = arg.slice(5).join(' ');
+
+      if (!arg || arg.length === 0) {
+        let mesg = `◇ *Pseudo👤*: ${data.e1}
+◇ *Division🛡️*: ${data.e2}
+◇ *Classe🏆*: ${data.e3}
+◇ *Rang XP🔰*: ${data.e4}
+◇ *Golds🧭*: ${data.e5}🧭
+◇ *NΞOcoins🔹*: ${data.e6}🔷
+◇ *Gift Box🎁*: ${data.e7}🎁
+◇ *Coupons🎟*: ${data.e8}🎟
+◇ *NΞO PASS🔸*: ${data.e9}🔸 
+*❯❯▓▓▓▓▓▓▓▓▓▓▓▓▓▓*
+ **🧠Talent Qi: ${data.e10}⭐* 
+*✅Clean games*:  ${data.e18}  *❌Mauvais PA:* ${data.e19}                             
+*👊🏻Close combat*: ${data.e20}     *🌀Attaques*: ${data.e21}
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*✭Records*: ${data.e12} Victoires✅/ ${data.e13} Défaites❌
+*🏆Trophées*: ${data.e14}  *🌟 TOS*: ${data.e15}  
+*💫Neo Awards*: ${data.e16}   *🎖️Globes*: ${data.e22}
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards*: ${data.e17} 
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+    *🔷𝗡Ξ𝗢 SUPERLEAGUE🏆🔝*`;
+zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/219e7e8d796b7b4e1ec9d.jpg' }, caption: mesg }, { quoted: ms });
+       } else {
+        if (superUser) { 
+     //   const dbUrl = "postgres://fatao:Kuz6KQRpz3S1swoTQTv1WOG8SPfSCppB@dpg-cmnlnkol5elc738lrj2g-a.oregon-postgres.render.com/cy";
+        const proConfig = {
+          connectionString: dbUrl,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+
+        const { Pool } = require('pg');
+        const pool = new Pool(proConfig);
+        const client = await pool.connect();
+
+        if (arg[0] === 'joueur:') {
+          let colonnesJoueur;
+          
+          switch (joueur) {
+    case "Aruto":
+      colonnesJoueur = {
+        pseudo: "e1",
+        division: "e2",
+        classe: "e3",
+        rang_exp: "e4",
+        golds: "e5",
+        neocoins: "e6",
+        gift_box: "e7",
+        coupons: "e8",
+        neopass: "e9",
+        talent: "e10",
+        victoires: "e12",
+        defaites: "e13",
+        trophees: "e14",
+        tos: "e15",
+        awards: "e16",
+        cards: "e17",
+        Clean_games: "e18",
+        Mauvais_pa: "e19",
+        Close_combat: "e20",
+        Attaques: "e21",
+        globes: "e22"
+      };
+        break;
+          default:
+      console.log("Nom de joueur non reconnu.");
+              repondre(`joueur: ${joueur} non reconnu`);
+              return; 
+        }
+          
+        const colonneObjet = colonnesJoueur[object];
+        const solde = `${data[colonneObjet]} ${signe} ${valeur}`;
+
+          if (colonneObjet && (signe === '+' || signe === '-')) {
+            const query = `UPDATE northdiv SET ${colonneObjet} = ${data[colonneObjet]} ${signe} ${valeur} WHERE id = 12`;
+            await client.query(query);
+
+            console.log(`Données de l'utilisateur ${joueur} mises à jour`);
+           await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${signe}${valeur}\n*NOUVEAU SOLDE*: ${solde}`);
+          } else if (colonneObjet && signe === '=') {
+            const query = `
+            UPDATE northdiv
+            SET ${colonneObjet} = $1
+            WHERE id = 12
+            `;
+
+            await client.query(query, [texte]);
+
+            console.log(`données du joueur: ${joueur} mise à jour`);
+            await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${texte} \n *NOUVELLE DONNÉE*: ${texte}`);
+          } else {
+            console.log("Nom d'objet non reconnu ou signe invalide.");
+            repondre(`Une erreur est survenue. Veuillez entrer correctement les données.`);
+          }
+        } else {
+          console.log("Le message ne correspond pas au format attendu.");
+          repondre(`Le format du message est incorrect.`);
+        } 
+        } else { repondre('Seul les Membres de la NS ont le droit de modifier cette fiche');}
+       
+
+        client.release();
+      }
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
+    }
+  });
+
+zokou(
+  {
+    nomCom: 'northeoza👤',
+    categorie: 'NEOverse'
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, repondre, arg, superUser } = commandeOptions;
+
+    try {
+      const data = await getData('13');
+      let joueur = arg[1];
+      let object = arg[3];
+      let signe = arg[4];
+      let valeur = arg[5];
+      let texte = arg.slice(5).join(' ');
+
+      if (!arg || arg.length === 0) {
+        let mesg = `◇ *Pseudo👤*: ${data.e1}
+◇ *Division🛡️*: ${data.e2}
+◇ *Classe🏆*: ${data.e3}
+◇ *Rang XP🔰*: ${data.e4}
+◇ *Golds🧭*: ${data.e5}🧭
+◇ *NΞOcoins🔹*: ${data.e6}🔷
+◇ *Gift Box🎁*: ${data.e7}🎁
+◇ *Coupons🎟*: ${data.e8}🎟
+◇ *NΞO PASS🔸*: ${data.e9}🔸 
+*❯❯▓▓▓▓▓▓▓▓▓▓▓▓▓▓*
+ **🧠Talent Qi: ${data.e10}⭐* 
+*✅Clean games*:  ${data.e18}  *❌Mauvais PA:* ${data.e19}                             
+*👊🏻Close combat*: ${data.e20}     *🌀Attaques*: ${data.e21}
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*✭Records*: ${data.e12} Victoires✅/ ${data.e13} Défaites❌
+*🏆Trophées*: ${data.e14}  *🌟 TOS*: ${data.e15}  
+*💫Neo Awards*: ${data.e16}   *🎖️Globes*: ${data.e22}
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards*: ${data.e17} 
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+    *🔷𝗡Ξ𝗢 SUPERLEAGUE🏆🔝*`;
+zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/4fc0cc36b556d4c7ad4d9.jpg' }, caption: mesg }, { quoted: ms });
+       } else {
+        if (superUser) { 
+     //   const dbUrl = "postgres://fatao:Kuz6KQRpz3S1swoTQTv1WOG8SPfSCppB@dpg-cmnlnkol5elc738lrj2g-a.oregon-postgres.render.com/cy";
+        const proConfig = {
+          connectionString: dbUrl,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+
+        const { Pool } = require('pg');
+        const pool = new Pool(proConfig);
+        const client = await pool.connect();
+
+        if (arg[0] === 'joueur:') {
+          let colonnesJoueur;
+          
+          switch (joueur) {
+    case "Eoza":
+      colonnesJoueur = {
+        pseudo: "e1",
+        division: "e2",
+        classe: "e3",
+        rang_exp: "e4",
+        golds: "e5",
+        neocoins: "e6",
+        gift_box: "e7",
+        coupons: "e8",
+        neopass: "e9",
+        talent: "e10",
+        victoires: "e12",
+        defaites: "e13",
+        trophees: "e14",
+        tos: "e15",
+        awards: "e16",
+        cards: "e17",
+        Clean_games: "e18",
+        Mauvais_pa: "e19",
+        Close_combat: "e20",
+        Attaques: "e21",
+        globes: "e22"
+      };
+        break;
+          default:
+      console.log("Nom de joueur non reconnu.");
+              repondre(`joueur: ${joueur} non reconnu`);
+              return; 
+        }
+          
+        const colonneObjet = colonnesJoueur[object];
+        const solde = `${data[colonneObjet]} ${signe} ${valeur}`;
+
+          if (colonneObjet && (signe === '+' || signe === '-')) {
+            const query = `UPDATE northdiv SET ${colonneObjet} = ${data[colonneObjet]} ${signe} ${valeur} WHERE id = 13`;
+            await client.query(query);
+
+            console.log(`Données de l'utilisateur ${joueur} mises à jour`);
+           await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${signe}${valeur}\n*NOUVEAU SOLDE*: ${solde}`);
+          } else if (colonneObjet && signe === '=') {
+            const query = `
+            UPDATE northdiv
+            SET ${colonneObjet} = $1
+            WHERE id = 13
+            `;
+
+            await client.query(query, [texte]);
+
+            console.log(`données du joueur: ${joueur} mise à jour`);
+            await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${texte} \n *NOUVELLE DONNÉE*: ${texte}`);
+          } else {
+            console.log("Nom d'objet non reconnu ou signe invalide.");
+            repondre(`Une erreur est survenue. Veuillez entrer correctement les données.`);
+          }
+        } else {
+          console.log("Le message ne correspond pas au format attendu.");
+          repondre(`Le format du message est incorrect.`);
+        } 
+        } else { repondre('Seul les Membres de la NS ont le droit de modifier cette fiche');}
+       
+
+        client.release();
+      }
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
+    }
+  });
+
+zokou(
+  {
+    nomCom: 'northlone👤',
+    categorie: 'NEOverse'
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, repondre, arg, superUser } = commandeOptions;
+
+    try {
+      const data = await getData('10');
+      let joueur = arg[1];
+      let object = arg[3];
+      let signe = arg[4];
+      let valeur = arg[5];
+      let texte = arg.slice(5).join(' ');
+
+      if (!arg || arg.length === 0) {
+        let mesg = `◇ *Pseudo👤*: ${data.e1}
+◇ *Division🛡️*: ${data.e2}
+◇ *Classe🏆*: ${data.e3}
+◇ *Rang XP🔰*: ${data.e4}
+◇ *Golds🧭*: ${data.e5}🧭
+◇ *NΞOcoins🔹*: ${data.e6}🔷
+◇ *Gift Box🎁*: ${data.e7}🎁
+◇ *Coupons🎟*: ${data.e8}🎟
+◇ *NΞO PASS🔸*: ${data.e9}🔸 
+*❯❯▓▓▓▓▓▓▓▓▓▓▓▓▓▓*
+ **🧠Talent Qi: ${data.e10}⭐* 
+*✅Clean games*:  ${data.e18}  *❌Mauvais PA:* ${data.e19}                             
+*👊🏻Close combat*: ${data.e20}     *🌀Attaques*: ${data.e21}
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*✭Records*: ${data.e12} Victoires✅/ ${data.e13} Défaites❌
+*🏆Trophées*: ${data.e14}  *🌟 TOS*: ${data.e15}  
+*💫Neo Awards*: ${data.e16}   *🎖️Globes*: ${data.e22}
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+*🎴Cards*: ${data.e17} 
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+    *🔷𝗡Ξ𝗢 SUPERLEAGUE🏆🔝*`;
+zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/5901ab31b72ad80f1c498.jpg' }, caption: mesg }, { quoted: ms });
+       } else {
+        if (superUser) { 
+     //   const dbUrl = "postgres://fatao:Kuz6KQRpz3S1swoTQTv1WOG8SPfSCppB@dpg-cmnlnkol5elc738lrj2g-a.oregon-postgres.render.com/cy";
+        const proConfig = {
+          connectionString: dbUrl,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+
+        const { Pool } = require('pg');
+        const pool = new Pool(proConfig);
+        const client = await pool.connect();
+
+        if (arg[0] === 'joueur:') {
+          let colonnesJoueur;
+          
+          switch (joueur) {
+    case "Lone":
+      colonnesJoueur = {
+        pseudo: "e1",
+        division: "e2",
+        classe: "e3",
+        rang_exp: "e4",
+        golds: "e5",
+        neocoins: "e6",
+        gift_box: "e7",
+        coupons: "e8",
+        neopass: "e9",
+        talent: "e10",
+        victoires: "e12",
+        defaites: "e13",
+        trophees: "e14",
+        tos: "e15",
+        awards: "e16",
+        cards: "e17",
+        Clean_games: "e18",
+        Mauvais_pa: "e19",
+        Close_combat: "e20",
+        Attaques: "e21",
+        globes: "e22"
+      };
+        break;
+          default:
+      console.log("Nom de joueur non reconnu.");
+              repondre(`joueur: ${joueur} non reconnu`);
+              return; 
+        }
+          
+        const colonneObjet = colonnesJoueur[object];
+        const solde = `${data[colonneObjet]} ${signe} ${valeur}`;
+
+          if (colonneObjet && (signe === '+' || signe === '-')) {
+            const query = `UPDATE northdiv SET ${colonneObjet} = ${data[colonneObjet]} ${signe} ${valeur} WHERE id = 14`;
+            await client.query(query);
+
+            console.log(`Données de l'utilisateur ${joueur} mises à jour`);
+           await repondre(`Données du joueur mises à jour\n👤 *JOUEUR*: ${joueur}\n⚙ *OBJECT*: ${object}\n💵 *VALEUR*: ${signe}${valeur}\n*NOUVEAU SOLDE*: ${solde}`);
+          } else if (colonneObjet && signe === '=') {
+            const query = `
+            UPDATE northdiv
+            SET ${colonneObjet} = $1
+            WHERE id = 14
             `;
 
             await client.query(query, [texte]);
