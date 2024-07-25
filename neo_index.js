597537bb36d162a6c2e5d58d5da0d9f4d27b77c8
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-07-25 21:44:44 +0000

    Update neo_index.js

--- neo_index.js
+++ neo_index.js
@@ -1,11 +1,12 @@
 async (dest, zk, commandeOptions) => {
-    const { origineMessage, tes, texte } = commandeOptions;
+    const { origineMessage, texte } = commandeOptions;
     try{
     
             // Vérifiez si le message provient du groupe spécifique
-           // if (origineMessage === '120363307444088356@g.us') {
+           if (texte && origineMessage === '22651463203@s.whatsapp.net') {
                 console.log("\t [][]...{neoverse_Md}...[][]");
-                console.log(tes);
+                console.log(texte);
+           }
             
     } catch (error) {
         console.error("Une erreur est survenue :", error);
