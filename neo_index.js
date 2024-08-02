f0ae83239eeedfd50577ae60c8c7692b79ca49ba
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-02 23:41:29 +0000

    Update neo_index.js

--- neo_index.js
+++ neo_index.js
@@ -1,7 +1,7 @@
-const { dest, zk, commandeOptions } = require('/index');
+const { origineMessage, zk, commandeOptions } = require('/index');
 
 const {repondre,ms,arg,texte}=commandeOptions;
 
-if (texte === 'salut' && dest === '22651463203@s.whatsapp.net')
+if (texte === 'salut' && origineMessage === '22651463203@s.whatsapp.net')
 { répondre('salut') 
 };
