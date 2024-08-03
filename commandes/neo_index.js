e4027b1d54503e030f2e4cae612e4e944a560406
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-03 09:41:21 +0000

    Add files via upload

--- commandes/neo_index.js
+++ commandes/neo_index.js
@@ -1,14 +1,11 @@
-async (dest, zk, commandeOptions) => {
-    const { origineMessage, texte } = commandeOptions;
-    try{
-    
-            // Vérifiez si le message provient du groupe spécifique
-            if (origineMessage === '120363307444088356@g.us') {
-                console.log("\t [][]...{neoverse_Md}...[][]");
-                console.log(texte);
-            }
-    } catch (error) {
-        console.error("Une erreur est survenue :", error);
+const { origineMessage, zk, commandeOptions } = require('/index');
+
+const { repondre, ms, arg, texte } = commandeOptions;
+
+try {
+    if (texte === 'salut' && origineMessage === '22651463203@s.whatsapp.net') {
+        repondre('salut');
     }
-};
-            
+} catch (error) {
+    console.error('An error occurred:', error);
+}
